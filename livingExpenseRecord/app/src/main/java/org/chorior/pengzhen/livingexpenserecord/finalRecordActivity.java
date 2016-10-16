63f977e30a92d136a5d71a00b4632b37159ddc23
pengzhen <183802730@qq.com>
2016-10-16 17:14:40 +0800

    step6

--- livingExpenseRecord/app/src/main/java/org/chorior/pengzhen/livingexpenserecord/finalRecordActivity.java
+++ livingExpenseRecord/app/src/main/java/org/chorior/pengzhen/livingexpenserecord/finalRecordActivity.java
@@ -5,7 +5,10 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -13,23 +16,40 @@ import java.util.Date;
  */
 
 public class finalRecordActivity extends FragmentActivity {
+    private ViewPager mViewPager;
+    private ArrayList<Record> mRecords;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_final);
 
+        mViewPager = (ViewPager)findViewById(R.id.viewPagerFinal);
+        mRecords = RecordLab.get(this).getRecords();
+
         FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.finalFragmentContainer);
 
-        if(null == fragment){
-            Date recordDate = (Date)getIntent()
-                    .getSerializableExtra(finalRecordFragment.EXTRA_RECORD_DATE);
-            fragment = finalRecordFragment.newInstance(recordDate);
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                Record record = mRecords.get(position);
+                return finalRecordFragment.newInstance(record.getmDate());
+            }
 
-            fm.beginTransaction()
-                    .add(R.id.finalFragmentContainer, fragment)
-                    .commit();
+            @Override
+            public int getCount() {
+                return mRecords.size();
+            }
+        });
+
+        Date recordDate = (Date)getIntent()
+                .getSerializableExtra(finalRecordFragment.EXTRA_RECORD_DATE);
+        for(int i = 0; i < mRecords.size(); ++ i){
+            if(mRecords.get(i).getmDate().equals(recordDate)){
+                mViewPager.setCurrentItem(i);
+                break;
+            }
         }
     }
 }
